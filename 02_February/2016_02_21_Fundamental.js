1c1a62e1f4888f5451dda47d9014d33661104e4f
Anthony Ng <newyork.anthonyng@gmail.com>
2016-02-21 09:00:27 -0500

    Add best answer

--- 02_February/2016_02_21_Fundamental.js
+++ 02_February/2016_02_21_Fundamental.js
@@ -1,6 +1,8 @@
 function removeChar(str){
-  var myArray = str.split('');
-  return myArray.slice(1, myArray.length - 1).join('');
+//  var myArray = str.split('');
+//  return myArray.slice(1, myArray.length - 1).join('');
+//  best answer. Remember that a string also has a slice method, similar to array.slice
+  return str.slice(1, -1);
 };
 
 console.log(removeChar('eloquent'));
